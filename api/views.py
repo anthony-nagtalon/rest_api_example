77fa7d883e7cc8558243aff14b902514054aba6d
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-07-28 16:57:03 -0700

    Added serializer and read methods

--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+from api import serializers
 
 # Create your views here.
 
@@ -15,4 +18,19 @@ def apiOverview(request):
         "Update" : "/task-update/<str:pk>/",
         "Delete" : "/task-delete/<str:pk>/"
     }
+
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id = pk)
+    serializer = TaskSerializer(tasks, many = False)
+    return Response(serializer.data)
+
+
