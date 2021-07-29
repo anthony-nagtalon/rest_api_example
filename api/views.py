842048b329a9326c3078d8bec821b8c7b3e6b5f6
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-07-29 01:42:26 -0700

    Populated db for testing

--- api/views.py
+++ api/views.py
@@ -17,7 +17,7 @@ def apiOverview(request):
         "Create" : "/task-create/",
         "Update" : "/task-update/<str:pk>/",
         "Delete" : "/task-delete/<str:pk>/"
-    }
+        }
 
     return Response(api_urls)
 
